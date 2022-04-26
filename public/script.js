2641c9517a672baf7f5035580cd6accbdb1330a9
Isabella09 <isagiraffe79@gmail.com>
2022-04-26 14:22:30 -0400

    did canvas

--- public/script.js
+++ public/script.js
@@ -18,3 +18,38 @@ socket.on('chat message', function(msg) {
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
+
+
+const canvas = document.getElementById("canvas");
+const c = canvas.getContext("2d");
+
+let pos = {};
+let mouseDown = false;
+document.addEventListener("mousemove", e => {
+    pos.x = e.clientX;
+    pos.y = e.clientY;
+    if(mouseDown){
+        c.beginPath();
+        draw()
+    } else {
+        c.closePath();
+    }
+})
+
+document.addEventListener("mousedown", (e) => {
+    console.log("mouse down")
+    mouseDown = true});
+document.addEventListener("mouseup", (e) => mouseDown = false);
+
+function draw (){
+
+    c.lineWidth = 5;
+    c.lineCap = 'round';
+    c.strokeStyle = '#c0392b';
+    
+    c.moveTo(pos.x, pos.y);
+    
+    c.lineTo(pos.x, pos.y);
+    
+    c.stroke();
+}
