f94815ee79aef9f725cd8bb2a75d1e367ced2f8f
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-04-26 12:24:41 -0400

    public folder

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
@@ -15,7 +12,12 @@ io.on('connection', (socket) => {
     });
 });
 
+app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+  
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
