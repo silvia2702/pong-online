9a4a3ccc9cbe8f15034804de92dc5607c5f5ccab
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-04-26 12:19:33 -0400

    fixed endenting

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
     });
-  });
+});
 
-  
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
